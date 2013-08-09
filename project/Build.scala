a373b99b1fb45ae1a4411187e0d3cfdaef59f009
FortyTwo <fortytwo@marvin.keep42.com>
2013-08-09 00:27:31 -0400

    change of publish repo path

--- project/Build.scala
+++ project/Build.scala
@@ -12,7 +12,7 @@ object Build extends sbt.Build {
 		crossScalaVersions := Seq("2.10.1"),
 		scalacOptions := Seq("-deprecation", "-encoding", "utf8"),
 		resolvers     ++= Dependencies.resolutionRepos,
-		publishTo := Some(Resolver.file("file", new File("../../ivy-repo/"))),
+		publishTo := Some(Resolver.file("file", new File("../../sonatype-work/nexus/storage/towel"))),
 		libraryDependencies ++=
 		  provided(akkaActor, slf4j, logback) ++ compile(quartz) ++
 		  test(specs2, akkaTestkit)
